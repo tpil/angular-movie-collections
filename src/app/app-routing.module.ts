ae06cc7957e8287a90f8aa8df70d9f3bcbfdf3e4
teopyl <teopyl@gmail.com>
2021-03-13 23:11:11 +0200

    Components, styling, routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {DetailsComponent} from './details/details.component';
+import {CollectionsComponent} from './collections/collections.component';
+import {SearchMoviesComponent} from './search-movies/search-movies.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', component:SearchMoviesComponent},
+{path:'collections', component:CollectionsComponent},
+{path:'details', component:DetailsComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
