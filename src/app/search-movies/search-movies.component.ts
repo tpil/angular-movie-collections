e947e2447d67ed0e24cb6478c961dd3818b02d29
teopyl <teopyl@gmail.com>
2021-03-14 18:28:13 +0200

    Fetch data from API

--- src/app/search-movies/search-movies.component.ts
+++ src/app/search-movies/search-movies.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpService} from '../http.service';
+import { HttpClient,HttpParams } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-search-movies',
@@ -8,19 +9,29 @@ import {HttpService} from '../http.service';
 })
 export class SearchMoviesComponent implements OnInit {
 
-  name: string='';
+  API_KEY:string='85204a8cc33baf447559fb6d51b18313';
+  term: string='';
   movies:any;
 
-  constructor(private _http: HttpService) { }
+  constructor(private _http: HttpClient) { }
 
   ngOnInit(): void {
     
   }
+
+  getMovies(){
+    let searchParams =new HttpParams();
+    searchParams =searchParams.append('language','en-US');
+    searchParams =searchParams.append('page','1');
+    searchParams =searchParams.append('include_adult','false');
+    searchParams =searchParams.append('query',this.term);
   
-  fetchMovies(){
-    this._http.getMovies().subscribe(data=>{
-      this.movies=data;
-      console.log(this.movies);
+    return this._http.get(`https://api.themoviedb.org/3/search/movie?api_key=${this.API_KEY}`,{
+      params:searchParams
+    }).subscribe(data=>{
+      this.movies=[data];
+      console.log( this.movies[0].results);
+      this.movies=this.movies[0].results;
     });
   }
 
